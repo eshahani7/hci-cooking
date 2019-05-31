3689d0a6af513aa18ca59249bfce3c64e8d09905
Ekta Shahani <shahani.ekta@gmail.com>
2019-05-31 13:41:35 -0700

    nav base

---
+++ README.md
@@ -0,0 +1,5 @@
+### Nav Instructions
+1. Include stylesheet under css/nav-styles.css.
+2. Include script under js/nav.js AND jquery minified CDN (see home.html)
+3. Place <div id="side-nav"></div> at the top of your page body.
+5/31: it's not done yet, can someone do the profile part?
---
+++ js/nav.js
@@ -0,0 +1,16 @@
+$(function(){
+  $("#side-nav").load("nav.html");
+});
+
+function open_nav() {
+  document.getElementById("main").style.marginLeft = "15%";
+  document.getElementById("mySidebar").style.width = "15%";
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("openNav").style.display = 'none';
+}
+
+function close_nav() {
+  document.getElementById("main").style.marginLeft = "0%";
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
+}
