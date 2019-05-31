91896f8eb1e109d19ccd0a999aeb9ddcf35e7ce2
Ekta Shahani <shahani.ekta@gmail.com>
2019-05-30 19:38:48 -0700

    signup success

--- js/login-script.js
+++ js/login-script.js
@@ -1,3 +1,6 @@
 function toggleFlip(elem) {
   document.getElementsByClassName("login-box")[0].classList.toggle("flipped");
+  if(elem.classList.contains("signup-btn")) {
+    document.getElementsByClassName("signup-success")[0].classList.add("visible");
+  }
 }
