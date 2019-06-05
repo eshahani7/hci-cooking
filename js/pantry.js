b1fc7b363581954bc39d19dea54016a1f0cecb41
Christina Tao <tina@ResNet-49-153.resnet.ucsb.edu>
2019-06-04 18:56:13 -0700

    pantry wip

--- js/pantry.js
+++ js/pantry.js
@@ -1,3 +1,13 @@
-function selectView() {
+function search() {
     
+}
+
+function selectView(btn) {
+    buttons = document.getElementsByClassName("view-options-btn");
+    for (i = 0; i < buttons.length; i++) {
+        buttons[i].style.backgroundColor = "white";
+        buttons[i].style.color = "black"
+    }
+    btn.style.backgroundColor = "#2EC4B6"
+    btn.style.color = "white";
 }
