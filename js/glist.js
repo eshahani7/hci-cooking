a0577ea9256ce4298fd88b4812d63a964cfac8f0
Ekta Shahani <shahani.ekta@gmail.com>
2019-06-05 22:19:20 -0700

    grocery list functional

--- js/glist.js
+++ js/glist.js
@@ -5,10 +5,10 @@ var addItem = document.getElementById("add").cloneNode(true);
 // adapted from https://stackoverflow.com/questions/27476566/how-to-make-li-editable-on-click
 $('.addNewContext').click(function() {
     $(this).html("").attr('contenteditable', 'true');
-    curr = this;
 }).keyup(function(e) {
   if (e.keyCode == 13) {
      var val = $(this).text();
+     var heading = this.id.split("-")[0];
      var listItem = document.getElementById("item");
      var itemClone = listItem.cloneNode(true);
      itemClone.removeAttribute("id");
@@ -19,13 +19,41 @@ $('.addNewContext').click(function() {
      $(this)
         // create a new li item
         .before(itemClone)
-        // set plus sign again
+        // insert add field
         .html(addItem)
         // make contenteditable to false, when clicked the process start again.
         .attr('contenteditable', 'false');
      e.preventDefault();
+
+     // save item to grocery list collection
+     addToList(val, heading);
   }
 });
+
+function onCheckChange(elem) {
+  var key = elem.parentNode.parentNode.querySelector(".item-label").innerText;
+  var value = elem.parentNode.parentNode.parentNode.parentNode.id;
+
+  if(elem.checked) {
+    removeFromList(key, value);
+  } else {
+    addToList(key, value);
+  }
+}
+
+function addToList(key, value) {
+  const update = {};
+  update[`groceryList.${key}`] = value;
+  db.collection("users").doc("eshahani").update(update);
+}
+
+function removeFromList(key, value) {
+  db.collection("users").doc("eshahani").get().then(function(doc) {
+    const map = doc.data().groceryList;
+    delete map[key];
+    db.collection("users").doc("eshahani").update({groceryList : map});
+  });
+}
 
 function loadItems() {
   db.collection("users").doc("eshahani").get().then(function(doc) {
@@ -46,10 +74,3 @@ function loadItems() {
     })
   });
 }
-
-// function removeFromList(elem) {
-//   var label = elem.parentNode.querySelector(".item-label").innerText;
-//   db.collection("users").doc("eshahani").get().then(function(doc) {
-//
-//   });
-// }
