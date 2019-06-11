49f89573763e3e621461ca97090ffda0214e53ae
Ekta Shahani <shahani.ekta@gmail.com>
2019-06-10 22:55:44 -0700

    fixes

--- js/login-script.js
+++ js/login-script.js
@@ -1,6 +1,15 @@
+var db = firebase.firestore();
+
 function toggleFlip(elem) {
   document.getElementsByClassName("login-box")[0].classList.toggle("flipped");
   if(elem.classList.contains("signup-btn")) {
     document.getElementsByClassName("signup-success")[0].classList.add("visible");
   }
+}
+
+function resetMealGen() {
+  db.collection("users").doc("eshahani").update({
+    mealGenerated: false
+  });
+  window.location = './home.html';
 }
